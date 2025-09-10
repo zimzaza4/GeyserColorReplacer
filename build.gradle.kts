82520684130d8897c1a92ff17643b9f71a89fa4e
zimzaza4 <3625282098@qq.com>
2025-09-10 21:19:53 +0800

    Remove useless depends

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 
-    maven("https://maven.elytrium.net/repo/")
     maven("https://repo.opencollab.dev/main/")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://oss.sonatype.org/content/groups/public/")
@@ -23,7 +22,6 @@ dependencies {
     annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     compileOnly("com.github.retrooper:packetevents-spigot:2.8.0")
     compileOnly("org.geysermc.floodgate:api:2.2.3-SNAPSHOT")
-    implementation("net.elytrium.commons:config:1.2.5-1")
 }
 
 tasks {
